4fbd8b27b1e6d97020b0f0df6754e7e3fe820f48
chuhangyu <1448949475@qq.com>
2020-09-16 21:30:20 +0800

    发布event

---
+++ src/index.js
@@ -0,0 +1,37 @@
+class Event {
+  constructor() {
+    this.handlers = {};
+  }
+
+  emit(msg) {
+    if (this.handlers[msg] && this.handlers[msg].length > 0) {
+      this.handlers[msg].forEach(cb => cb());
+    }
+  }
+
+  on(msg, cb) {
+    if (!this.handlers[msg]) {
+      this.handlers[msg] = [ cb ];
+    } else {
+      this.handlers[msg].push(cb);
+    }
+  }
+
+  remove(msg, cb) {
+    if (this.handlers[msg]) {
+      this.handlers[msg].splice(this.handlers[msg].indexOf(cb), 1);
+    }
+  }
+}
+
+const createEvent = (function() {
+  let instance;
+  return function() {
+    if (!instance) {
+      instance = new Event();
+    }
+    return instance;
+  }
+})();
+
+module.exports = createEvent;
